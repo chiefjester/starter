43e4a218d903fafff2e8706f7c1e1a5e5d6b20c6
chris <532797+thisguychris@users.noreply.github.com>
2024-01-05 03:57:17 +0800

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+---- make oil work like vim-vinegar
+
+-- make oil work like vim-vinegar
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- general enhancements
+vim.keymap.set("n", "<leader>q", ":silent q<cr>", { desc = "Close Window", silent = true })
+vim.keymap.set("n", "<leader>z", ":silent update<cr>", { desc = "Save", silent = true })
+vim.keymap.set("n", "<leader>o", ":only<cr>", { desc = "Only", silent = true })
+vim.keymap.set("n", "<leader>gs", ":Git<cr>", { desc = "Git Status", silent = true })
