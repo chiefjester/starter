08828468fdd290d2bf208b5ad1ca3c04cd68d1c9
chris <532797+thisguychris@users.noreply.github.com>
2024-01-05 03:59:07 +0800

    add options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+vim.o.guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
+vim.opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
+
+-- fold settings
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldlevel = 99
+vim.wo.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) ]]
+vim.wo.fillchars = "fold: "
+vim.wo.foldnestmax = 3
+vim.wo.foldminlines = 1
+
+vim.o.splitbelow = false
+
+-- vim.api.nvim_set_hl(0, "Normal", { ctermfg = White, ctermbg = Black })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#1a1a1a" })
