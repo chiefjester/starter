b1823ab6be9a953effd7f5b796440de67374f572
chris <532797+thisguychris@users.noreply.github.com>
2024-01-05 05:33:48 +0800

    add persistence.nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "nvimdev/dashboard-nvim", enabled = false },
-  { "lazy/noice.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
 }
